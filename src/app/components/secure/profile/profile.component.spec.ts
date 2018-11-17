ca67a78e898e3e0b9cd7e47db406d42fcd3501da
jmilleriec <jmilleriec.dev@gmail.com>
2018-11-17 13:30:18 -0500

    Tweaks to profile tests

--- src/app/components/secure/profile/profile.component.spec.ts
+++ src/app/components/secure/profile/profile.component.spec.ts
@@ -4,29 +4,35 @@ import { ProfileComponent } from './profile.component';
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from 'selenium-webdriver/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterModule } from '@angular/router';
+import { ProfileService } from 'src/app/services/customer/profile';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ],
-      imports: [HttpModule, FormsModule],
-      providers: [HttpClient]
-      
-    })
-    .compileComponents();
-  }));
+  let service: ProfileService;
+  let spy: any;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProfileComponent ],
+      providers: [ RouterModule, ProfileService ],
+      imports: [ RouterModule, RouterTestingModule,FormsModule,
+        RouterTestingModule.withRoutes([]),
+        HttpClientTestingModule]
+    })
+    service = TestBed.get(ProfileService);
     fixture = TestBed.createComponent(ProfileComponent);
     component = fixture.componentInstance;
     //fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should retrieve profile attributes for the current user', () => {
+    expect(component).toBeTruthy();
+  });
+  it('should update profile attributes for the current user', () => {
     expect(component).toBeTruthy();
   });
 });
