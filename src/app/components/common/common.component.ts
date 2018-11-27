c2d35bfafb2d9c81fce01f51b97e6eec6d1d448e
columbusdevsayshello <43658289+columbusdevsayshello@users.noreply.github.com>
2018-11-27 18:16:29 -0500

    Fixing small issue.

--- src/app/components/common/common.component.ts
+++ src/app/components/common/common.component.ts
@@ -16,21 +16,12 @@ export class CommonComponent implements OnInit {
   protected stContent: String[] = [];
   protected rtrnContent: String[][];
   constructor(protected http: HttpClient, 
-    protected contentSrv: ContentAPIService,
-    //protected passedContent: String[]
-    ) {
-    // this.stContent = passedContent;
-    // console.log(passedContent);
+    protected contentSrv: ContentAPIService) {
+
   }
 
   ngOnInit() {
-    // Get Externalized Content
-    //this.contentSrv.contentKeys=this.passedContent;
-    // this.contentSrv.getContent(this.stContent.join('&')).subscribe((ndata) => {
-    //   this.rtrnContent = this.contentSrv.parseContent(ndata);
-      //console.log('Content: ' + this.rtrnContent);
-    // }
-    // )
+  
   }
   public getViewContent(passedContent: String[]): any {
     this.stContent = passedContent;
